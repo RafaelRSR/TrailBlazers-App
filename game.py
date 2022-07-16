0b81cf7bf8dd9189aced87b1aad888eee0a0b5f5
Rafael Rocha <rafarocha1511@gmail.com>
2022-07-16 19:27:00 -0300

    Adding next games.

--- game.py
+++ game.py
@@ -1,8 +1,16 @@
+import next_games
+
 print("Welcome to the official Portland Trail Blazers App!")
 
 option = input("How can we help you today?\n 1 - Next Games\n 2 - Season Tickets\n 3 - Store\n")
 
+    
 if option == "1":
-    print("Nice")
-else: 
+    print(next_games.next_games)
+elif option == "2":
     print ("Not available.")
+elif option == "3":
+    print("Coming soon. You can check https://www.nba.com/blazers/ripcityclothing/ for online shopping!")
+else:
+    print("Option not available.")
+    
