7bfc6bf0f8ee77fdcaf41333a0bdc0280fda80bc
Rafael <rafarocha1511@gmail.com>
2022-07-16 18:58:39 -0300

    Adding options

---
+++ game.py
@@ -0,0 +1,8 @@
+print("Welcome to the official Portland Trail Blazers App!")
+
+option = input("How can we help you today?\n 1 - Next Games\n 2 - Season Tickets\n 3 - Store\n")
+
+if option == "1":
+    print("Nice")
+else: 
+    print ("Not available.")
